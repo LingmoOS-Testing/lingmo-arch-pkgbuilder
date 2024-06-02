56b631bc5376ff90870b82c4789d879bfdec43ef
Intro <1243710035@qq.com>
2024-06-02 20:31:16 +0800

    new file:   .vscode/settings.json 	deleted:    LingmoUI/LingmoUI.tar.xz 	modified:   LingmoUI/PKGBUILD 	modified:   build.sh 	modified:   liblingmo/PKGBUILD 	new file:   liblingmo/liblingmo-1.0.0-0-x86_64.pkg.tar.zst 	new file:   liblingmo/liblingmo-debug-1.0.0-0-x86_64.pkg.tar.zst 	new file:   liblingmo/liblingmo.tar.gz 	deleted:    liblingmo/liblingmo.tar.xz 	new file:   liblingmo/sourceURL 	deleted:    lingmo-baloo/PKGBUILD 	deleted:    lingmo-baloo/lingmo-baloo.tar.xz 	modified:   lingmo-calculator/PKGBUILD 	deleted:    lingmo-calculator/lingmo-calculator.tar.xz 	new file:   lingmo-calculator/sourceURL 	modified:   lingmo-core/PKGBUILD 	deleted:    lingmo-core/lingmo-core.tar.xz 	new file:   lingmo-core/sourceURL 	modified:   lingmo-cursor-themes/PKGBUILD 	deleted:    lingmo-cursor-themes/lingmo-cursor-themes.tar.xz 	new file:   lingmo-cursor-themes/sourceURL 	modified:   lingmo-dock/PKGBUILD 	new file:   lingmo-dock/lingmo-dock-main.tar.gz 	deleted:    lingmo-dock/lingmo-dock.tar.xz 	new file:   lingmo-dock/sourceURL 	modified:   lingmo-filemanager/PKGBUILD 	deleted:    lingmo-filemanager/lingmo-filemanager.tar.xz 	new file:   lingmo-filemanager/sourceURL 	modified:   lingmo-fontpkg/PKGBUILD 	deleted:    lingmo-fontpkg/lingmo-fontpkg.tar.xz 	modified:   lingmo-gtk-themes/PKGBUILD 	deleted:    lingmo-gtk-themes/lingmo-gtk-themes.tar.xz 	deleted:    lingmo-kwin-plugins/PKGBUILD 	modified:   lingmo-launcher/PKGBUILD 	deleted:    lingmo-launcher/lingmo-launcher.tar.xz 	modified:   lingmo-qt-plugins/PKGBUILD 	deleted:    lingmo-qt-plugins/lingmo-qt-plugins.tar.xz 	modified:   lingmo-screenlocker/PKGBUILD 	deleted:    lingmo-screenlocker/lingmo-screenlocker.tar.xz 	modified:   lingmo-screenshots/PKGBUILD 	deleted:    lingmo-screenshots/lingmo-screenshots.tar.xz 	modified:   lingmo-sddm-theme/PKGBUILD 	deleted:    lingmo-sddm-theme/lingmo-sddm-theme.tar.xz 	modified:   lingmo-settings/PKGBUILD 	deleted:    lingmo-settings/lingmo-settings.tar.xz 	modified:   lingmo-statusbar/PKGBUILD 	deleted:    lingmo-statusbar/lingmo-statusbar.tar.xz 	modified:   lingmo-systemicons/PKGBUILD 	deleted:    lingmo-systemicons/lingmo-systemicons.tar.xz 	modified:   lingmo-terminal/PKGBUILD 	deleted:    lingmo-terminal/lingmo-terminal.tar.xz 	modified:   lingmo-texteditor/PKGBUILD 	deleted:    lingmo-texteditor/lingmo-texteditor.tar.xz 	modified:   lingmo-videoplayer/PKGBUILD 	deleted:    lingmo-videoplayer/lingmo-videoplayer.tar.xz 	modified:   lingmo-wallpapers/PKGBUILD 	deleted:    lingmo-wallpapers/lingmo-wallpapers.tar.xz 	modified:   pkgs

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 while IFS= read -r target; do
     cd "$target"
+    sourceURL=$(head -n 1 sourceURL)
+    curl -Lko "$target.tar.gz" "$sourceURL"
     echo "Processing target: $target"
     makepkg -cfs --noconfirm
     cd ..
